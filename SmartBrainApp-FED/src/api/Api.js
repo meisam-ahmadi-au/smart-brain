b7cbfa4747b80728d29bec7bb49474b6a85ec147
DESKTOP-PULU5LM\Meisam <meisam.ahmadi.au@gmail.com>
2019-11-27 22:19:17 +1100

    fix typo

--- SmartBrainApp-FED/src/api/Api.js
+++ SmartBrainApp-FED/src/api/Api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http//localhost:3000";
+const baseUrl = "http://localhost:3000";
 class Api {
   static fetchImageUrl = (input) => fetch(`${baseUrl}/imageurl`, {
     method: 'post',
